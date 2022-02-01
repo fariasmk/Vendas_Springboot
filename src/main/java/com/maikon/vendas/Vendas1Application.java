b0b7b4504c7b8b855e01f7643fedfee253fc0ce1
fariasmk <fariasmk@hotmail.com>
2022-02-01 16:39:44 -0300

    Vers. 1.1

--- src/main/java/com/maikon/vendas/Vendas1Application.java
+++ src/main/java/com/maikon/vendas/Vendas1Application.java
@@ -16,41 +16,41 @@ public class Vendas1Application {
 
 	@Bean
 	public CommandLineRunner init(@Autowired ClientesRepository clientes) {
-        return args -> {
-            System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Maikon"));
-            clientes.salvar(new Cliente("Diego"));
+		return args -> {
+			System.out.println("Salvando clientes");
+			clientes.salvar(new Cliente("Maikon"));
+			clientes.salvar(new Cliente("Diego"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
+			System.out.println("Atualizando clientes");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
 
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("Cli").forEach(System.out::println);
 
-//            System.out.println("deletando clientes");
-//            clientes.obterTodos().forEach(c -> {
-//                clientes.deletar(c);
-//            });
+			System.out.println("deletando clientes");
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c);
+			});
 
-            todosClientes = clientes.obterTodos();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
-        };
-    }
+			todosClientes = clientes.obterTodos();
+			if (todosClientes.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado.");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
+		};
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Vendas1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Vendas1Application.class, args);
+	}
 }
--- src/main/java/com/maikon/vendas/domain/repositorio/ClientesRepository.java
+++ src/main/java/com/maikon/vendas/domain/repositorio/ClientesRepository.java
@@ -1,68 +1,59 @@
 package com.maikon.vendas.domain.repositorio;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.maikon.vendas.domain.entity.Cliente;
 
 @Repository
 public class ClientesRepository {
 
-	private static String INSERT = "insert into cliente (nome) values (?) ";
-	private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
-    private static String UPDATE = "update cliente set nome = ? where id = ? ";
-    private static String DELETE = "delete from cliente where id = ? ";
-    
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
+	private EntityManager entityManager;
 
+	@Transactional
 	public Cliente salvar(Cliente cliente) {
-		jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
+		entityManager.persist(cliente);
 		return cliente;
 	}
-	
-    public void deletar(Cliente cliente){
-        deletar(cliente.getId());
-    }
 
-    public void deletar(Integer id){
-        jdbcTemplate.update(DELETE, new Object[]{id});
-    }
-	
-    public Cliente atualizar(Cliente cliente){
-        jdbcTemplate.update(UPDATE, new Object[]{
-                cliente.getNome(), cliente.getId()} );
-        return cliente;
-    }
-    
-    @SuppressWarnings("deprecation")
-	public List<Cliente> buscarPorNome(String nome){
-        return jdbcTemplate.query(
-                SELECT_ALL.concat(" where nome like ? "),
-                new Object[]{"%" + nome + "%"},
-                obterClienteMapper());
-    }
-    
-    public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
-    }
-    
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                return new Cliente(id, nome);
-            }
-        };
-    }
+	@Transactional
+	public Cliente atualizar(Cliente cliente) {
+		entityManager.merge(cliente);
+		return cliente;
+	}
 
+	@Transactional
+	public void deletar(Cliente cliente) {
+		if (!entityManager.contains(cliente)) {
+			cliente = entityManager.merge(cliente);
+		}
+		entityManager.remove(cliente);
+	}
+
+	@Transactional
+	public void deletar(Integer id) {
+		Cliente cliente = entityManager.find(Cliente.class, id);
+		deletar(cliente);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Cliente> buscarPorNome(String nome) {
+		String jpql = " select c from Cliente c where c.nome like :nome ";
+		TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+		query.setParameter("nome", "%" + nome + "%");
+		return query.getResultList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Cliente> obterTodos() {
+		return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+	}
 
 }
