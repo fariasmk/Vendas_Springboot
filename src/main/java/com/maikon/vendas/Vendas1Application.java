b1f8ee445a79adafc2b20c001cad0e52381b9b5a
fariasmk <fariasmk@hotmail.com>
2022-02-02 14:30:52 -0300

    Vers. 1.4

--- src/main/java/com/maikon/vendas/Vendas1Application.java
+++ src/main/java/com/maikon/vendas/Vendas1Application.java
@@ -1,10 +1,24 @@
 package com.maikon.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.maikon.vendas.domain.entity.Cliente;
+import com.maikon.vendas.domain.repository.ClientesRepository;
 
 @SpringBootApplication
 public class Vendas1Application {
+	
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Vendas1Application.class, args);
--- src/main/java/com/maikon/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/maikon/vendas/rest/controller/ClienteController.java
@@ -1,25 +1,36 @@
 package com.maikon.vendas.rest.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.maikon.vendas.domain.entity.Cliente;
+import com.maikon.vendas.domain.repository.ClientesRepository;
 
 @Controller // Passa a ser controlada pela camada de injeção controller do Spring.
 public class ClienteController {
 
-	@RequestMapping(value = { "/api/clientes/hello/{nome}", "/api/hello" }, //Dessa forma podem ser acessadas ambas as urls.
-					method = RequestMethod.POST, 							//Determina o método REST.
-					consumes = { "application/json", "application/xml" },   //Define o tipo de conteúdo que o método pode receber. 
-					produces = { "application/json", "application/xml" })	//Define o tipo de conteúdo que o método pode retornar.
-	
-	@ResponseBody
-	public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
-		return String.format("Hello %s ", nomeCliente);
+	private ClientesRepository clientes;
+
+	public ClienteController(ClientesRepository  clientes) {
+		this.clientes = clientes;
 	}
 
+	@SuppressWarnings("rawtypes")
+	@GetMapping("/api/clientes/{id}") //Obter Dados.
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable Integer id ){ //@PathVariable especifica que o id vai estar na url.
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() ); // Retorna Status 200.
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
+
