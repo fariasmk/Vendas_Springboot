70fc8896e2f74baf11468777f5754ea4091e4c6c
fariasmk <fariasmk@hotmail.com>
2022-02-08 15:46:51 -0300

    Ver. 2.3

--- src/main/java/com/maikon/vendas/Vendas1Application.java
+++ src/main/java/com/maikon/vendas/Vendas1Application.java
@@ -2,9 +2,10 @@ package com.maikon.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Vendas1Application {
+public class Vendas1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Vendas1Application.class, args);
